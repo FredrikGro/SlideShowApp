18368cc6b0b6b52da93ad639099263979cca7b16
LuminTompa <lumincob@gmail.com>
2022-09-23 14:00:04 +0200

    add buttons to navigate to other screens

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Button, Text, View } from "react-native";
 import { styles } from "../styles";
 
-export default function Home() {
+type Props = NativeStackScreenProps<RootStackParamList, "Home">;
+
+export default function Home({ navigation }: Props) {
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <Button title="Sign In" onPress={() => navigation.navigate("SignIn")} />
+      <Button title="Log In" onPress={() => navigation.navigate("LogIn")} />
     </View>
   );
 }
