fa1f2e48078206ade4e1a037047745fbb3d084bc
LuminTompa <lumincob@gmail.com>
2022-09-23 13:23:45 +0200

    add navigations for all current screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import Home from "./screens/HomeScreen";
+import LogIn from "./screens/LogInScreen";
+import NewProjectScreen from "./screens/NewProjectScreen";
+import ProjectNavigation from "./screens/ProjectNavigationScreen";
+import Projects from "./screens/ProjectsScreen";
+import SignIn from "./screens/SignInScreen";
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+export type RootStackParamList = {
+  HomeScreen: undefined;
+  LoginScreen: undefined;
+  NewProjectScreen: undefined;
+  ProjectNavigationScreen: undefined;
+  ProjectsScreen: undefined;
+  SignInScreen: undefined;
+};
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="LogIn" component={LogIn} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="ProjectNavigation" component={ProjectNavigation} />
+        <Stack.Screen name="NewProject" component={NewProjectScreen} />
+        <Stack.Screen name="Projects" component={Projects} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
