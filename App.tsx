dcec49fcdd5d414f7b351e9f60f08ae2b11f1423
LuminTompa <lumincob@gmail.com>
2022-09-28 14:25:22 +0200

    add projectprovider

--- App.tsx
+++ App.tsx
@@ -2,15 +2,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import HeaderMenuButton from "./components/Button/HeaderMenuButton";
+import { colors } from "./components/colors";
+import ProjectProvider from "./contexts/ProjectContext";
 import Home from "./screens/HomeScreen";
 import LogIn from "./screens/LogInScreen";
 import NewProject from "./screens/NewProjectScreen";
+import ProjectName from "./screens/ProjectNameScreen";
 import ProjectNavigation from "./screens/ProjectNavigationScreen";
 import Projects from "./screens/ProjectsScreen";
 import SignIn from "./screens/SignInScreen";
-import { colors } from "./components/colors";
-import HeaderMenuButton from "./components/Button/HeaderMenuButton";
-import ProjectName from "./screens/ProjectNameScreen";
 
 const { primary, secondary } = colors;
 
@@ -35,42 +36,44 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <StatusBar style="auto" backgroundColor={primary} />
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={({ route }) => ({
-            headerTitle: route.name,
-            headerStyle: {
-              backgroundColor: secondary,
-            },
-          })}
-        >
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="LogIn" component={LogIn} />
-          <Stack.Screen name="SignIn" component={SignIn} />
-          <Stack.Screen
-            name="ProjectNavigation"
-            component={ProjectNavigation}
-            options={{ headerRight: () => <HeaderMenuButton /> }}
-          />
-          <Stack.Screen
-            name="NewProject"
-            component={NewProject}
-            options={{ headerRight: () => <HeaderMenuButton /> }}
-          />
-          <Stack.Screen
-            name="ProjectName"
-            component={ProjectName}
-            options={{ headerRight: () => <HeaderMenuButton /> }}
-          />
-          <Stack.Screen
-            name="Projects"
-            component={Projects}
-            options={{ headerRight: () => <HeaderMenuButton /> }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ProjectProvider>
+        <NavigationContainer>
+          <StatusBar style="auto" backgroundColor={primary} />
+          <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={({ route }) => ({
+              headerTitle: route.name,
+              headerStyle: {
+                backgroundColor: secondary,
+              },
+            })}
+          >
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="LogIn" component={LogIn} />
+            <Stack.Screen name="SignIn" component={SignIn} />
+            <Stack.Screen
+              name="ProjectNavigation"
+              component={ProjectNavigation}
+              options={{ headerRight: () => <HeaderMenuButton /> }}
+            />
+            <Stack.Screen
+              name="NewProject"
+              component={NewProject}
+              options={{ headerRight: () => <HeaderMenuButton /> }}
+            />
+            <Stack.Screen
+              name="ProjectName"
+              component={ProjectName}
+              options={{ headerRight: () => <HeaderMenuButton /> }}
+            />
+            <Stack.Screen
+              name="Projects"
+              component={Projects}
+              options={{ headerRight: () => <HeaderMenuButton /> }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ProjectProvider>
     </SafeAreaProvider>
   );
 }
