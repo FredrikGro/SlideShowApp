50caea3de4ae98b63261980b61d286be36e647ea
LuminTompa <lumincob@gmail.com>
2022-10-03 12:04:26 +0200

    add navigation to ProjectName

--- components/ProjectView.tsx
+++ components/ProjectView.tsx
@@ -1,16 +1,20 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { View } from "react-native";
+import { DrawerParamList } from "../Navigation/Drawer/DrawerNagivator";
 import RegularButton from "./Button/RegularButton";
 import RegularText from "./Texts/RegularText";
 
-export default function ProjectView() {
+type Props = NativeStackScreenProps<DrawerParamList, "Projects">;
+
+export default function ProjectView({navigation}: Props) {
   const { projects: [] } = useProject();
 
   return (
     {projects.length <= 0} ? (
         <View>
             <RegularText children={"You don't have any projects, head to Create new project if you wish to create a new one!"} />
-            <RegularButton children={"Create new project"} onPress={undefined} />
+            <RegularButton children={"Create new project"} onPress={() => navigation.navigate("ProjectName")} />
         </View>
     ) : (
         <View></View>
