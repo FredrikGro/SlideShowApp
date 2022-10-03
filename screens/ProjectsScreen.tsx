36176cb4b89cd73fc35be9c46b73a38a411dda06
LuminTompa <lumincob@gmail.com>
2022-10-03 12:59:20 +0200

    remove unnecessary stuff

--- screens/ProjectsScreen.tsx
+++ screens/ProjectsScreen.tsx
@@ -1,15 +1,5 @@
-import { StyleSheet } from "react-native";
 import ProjectView from "../components/ProjectView";
 
 export default function Projects() {
   return <ProjectView />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
