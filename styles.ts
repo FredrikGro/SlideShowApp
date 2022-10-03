d18e12fbabcbc580d549bb2d2dc931d6fdc278e1
LuminTompa <lumincob@gmail.com>
2022-10-03 12:25:13 +0200

    add new style class

--- styles.ts
+++ styles.ts
@@ -7,6 +7,12 @@ export const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  containerTop: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
   mb40: {
     marginBottom: 40,
   },
