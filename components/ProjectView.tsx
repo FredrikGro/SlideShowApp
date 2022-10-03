ad44c7009e0b67d15337d56dfb53f3e35e6a7adb
LuminTompa <lumincob@gmail.com>
2022-10-03 12:17:44 +0200

    add style to the views

--- components/ProjectView.tsx
+++ components/ProjectView.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { View } from "react-native";
 import { useProject } from "../contexts/ProjectContext";
+import { styles } from "../styles";
 import RegularText from "./Texts/RegularText";
 
 export default function ProjectView() {
   const { projects } = useProject();
 
   return projects.length <= 0 ? (
-    <View>
+    <View style={styles.container}>
       <RegularText
         children={
           "You don't have any projects, head to New Project if you wish to create a new one!"
@@ -15,7 +16,7 @@ export default function ProjectView() {
       />
     </View>
   ) : (
-    <View>
+    <View style={styles.container}>
       <RegularText children={"Your projects to be shown here"} />
     </View>
   );
