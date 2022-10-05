732f85b831d7636a151af35b37afe663d31a6027
Fredrik Gröndahl <contact@fredrikgrondahl.com>
2022-10-05 13:35:57 +0200

    Edit RegularButton style

--- components/Button/RegularButton.tsx
+++ components/Button/RegularButton.tsx
@@ -7,22 +7,28 @@ import RegularText from "../Texts/RegularText";
 
 import { colors } from "../colors";
 
-const { primary, accent} = colors;
+const { primary, accent } = colors;
 
 const ButtonView = styled.TouchableOpacity`
   background-color: ${accent};
   width: 300px;
   height: 60px;
-  border-radius: 15px;
+  border-radius: 25px;
   padding: 15px;
   justify-content: center;
   align-items: center;
+  shadow-opacity: 0.8;
+  elevation: 6;
+  shadow-radius: 15px;
+  shadow-offset: 1px 5px;
 `;
 
 const RegularButton: FunctionComponent<ButtonProps> = (props) => {
   return (
     <ButtonView onPress={props.onPress} style={props.style}>
-      <RegularText style={[{color: primary}, props.textStyle]}>{props.children}</RegularText>
+      <RegularText style={[{ color: primary }, props.textStyle]}>
+        {props.children}
+      </RegularText>
     </ButtonView>
   );
 };
